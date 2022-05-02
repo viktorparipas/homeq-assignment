5391ae8893c90a85cabc8c548678b8282132c148
Paripás Viktor <viktor.paripas@prorenata.se>
2022-05-02 23:13:50 +0200

    :star: Specify authentication methods

--- service/api/settings.py
+++ service/api/settings.py
@@ -128,7 +128,9 @@ ASSETS_BASE = "https://homeq-assets.s3.eu-north-1.amazonaws.com/coding-interview
 
 REST_FRAMEWORK = {
    'DEFAULT_AUTHENTICATION_CLASSES': (
-       'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
    ),
    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAdminUser'
